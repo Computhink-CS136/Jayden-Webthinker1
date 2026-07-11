eddf4a709d72bb3c892cb61f671c7844e0ba931c
Computhink-CS136 <cs136@computhink.com.sg>
2026-07-11 06:37:13 +0000

    Jul 11, 2026, 6:37 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -4,7 +4,8 @@ Task 1: Add the setup() function
 
 function setup1() {
   createCanvas(600, 400);
-  background
+  background(67)
+  
   // Challenge 1: An eye using ellipse() function
 
   // Challenge 2: A face using circle() function
