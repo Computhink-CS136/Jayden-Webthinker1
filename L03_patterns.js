39f45a0556f3c8602245f320962afe67c4718bbe
Computhink-CS136 <cs136@computhink.com.sg>
2026-07-18 06:47:49 +0000

    Jul 18, 2026, 6:47 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -9,7 +9,7 @@ function draw() {
 circle(75, 200, 100)
 circle(255, 200, 100)
 circle(375, 200, 100)
-circle(75, 200, 100)
+circle(525, 200, 100)
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
