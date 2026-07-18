ab721e5049a58ca7f6915f670b4be736185325e9
Computhink-CS136 <cs136@computhink.com.sg>
2026-07-18 06:11:19 +0000

    Jul 18, 2026, 6:11 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -34,6 +34,7 @@ quad(200, 100, 400, 100, 400, 300, 200, 300)
 circle(300, 200, 50)
 ellipse(250, 150, 40, 30)
 ellipse(350, 150, 40, 30)
+React()
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
