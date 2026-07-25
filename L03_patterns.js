44e7f7445f9248fd473681338ab6a58b0b2b6de0
Computhink-CS136 <cs136@computhink.com.sg>
2026-07-25 06:54:29 +0000

    Jul 25, 2026, 6:54 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -37,7 +37,7 @@ function draw() {
   // }
   // Task 4: Grid of Circles
   for (row = 0; row < 5; row++)
-    for(let column = 0; col <= 5; col++) {
+    for(let col = 0; col <= 5; col++) {
     if (i % 2 ==0) {
       fill(0); 
     } else {
