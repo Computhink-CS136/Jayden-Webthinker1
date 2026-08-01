cca826c03369f495cef003ba9ad648a730e48a52
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-01 06:57:17 +0000

    Aug 1, 2026, 6:57 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -43,3 +43,12 @@
 //     fill(randomCol)
 //     ellipse(mouseX, mouseY, 10, 10);
 // }
+function setup() {
+    createCanvas(400, 400);
+    background(220);
+}
+function mouseMoved() {
+    let randomCol = color(random(0, 255), random(0, 255), random(0, 255));
+    fill(randomCol)
+    ellipse(mouseX, mouseY, 10, 10);
+}
