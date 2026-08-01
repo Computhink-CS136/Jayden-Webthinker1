44d13f353e54c148db2c7d4da015abd0d9322698
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-01 06:24:48 +0000

    Aug 1, 2026, 6:24 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -12,8 +12,8 @@ function draw() {
 
 function mousePressed() {
     let randomCol = color(random(0, 255), random(0, 255), random(0, 255));
-        fill(random)
-    shapeColor = 'red';
+    fill(randomCol)
+    s
 }
 
 function mouseReleased() {
