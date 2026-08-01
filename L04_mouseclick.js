802c73bc833e0711f423fb2e3fb5b1eb019b684a
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-01 06:39:07 +0000

    Aug 1, 2026, 6:39 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -20,22 +20,3 @@
 //     shapeColor = 'blue';
 //     ellipse(200, 200, 80)
 // }
-
-function setup() {
-    createCanvas(400, 400);
-    background(220);
-}
-
-function draw() {
-    fill(shapeColor);
-}
-function mousePressed() {
-    let randomCol = color(random(0, 255), random(0, 255), random(0, 255));
-    fill(randomCol);
-    circleSize = random(100);
-    ellipse(200, 200, 80, 80);
-}
-function mouseReleased() {
-    shapeColor = 'white';
-    ellipse(200, 200, 80)
-}
