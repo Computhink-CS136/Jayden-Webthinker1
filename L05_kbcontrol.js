6d475181c41ff86923c7f3033c621b30b79e11f4
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-08 06:49:00 +0000

    Aug 8, 2026, 6:49 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -18,7 +18,7 @@ function setup() {
 //     rectSize = 50;
 // }
 function keyPressed() {
-    number = random(10, )
+    number = random(10, 20)
     for (let i = 0; i < number; i++)
         let Size = random(10, 50)
         let circle_col = color(random(0, 255), random(0, 255), random(0, 255))
