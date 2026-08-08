35ff15968465819c98228bb398c883dfca6dff4a
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-08 06:14:22 +0000

    Aug 8, 2026, 6:14 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -2,5 +2,9 @@ let rectSize = 50;
 
 function setup() {
     createCanvas(400, 400);
+}
 
+function draw() {
+    background(220);
+    rect(width / 2 - rectSize / 2, height / 2 - rectSize / 2, rectSize, rectSize);
 }
