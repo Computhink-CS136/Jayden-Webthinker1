7a55621e3a549a45c2e8bdd40fae93820a9a4f07
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-08 07:01:46 +0000

    Aug 8, 2026, 7:01 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -17,18 +17,18 @@ function setup() {
 // function keyReleased() {
 //     rectSize = 50;
 // }
-function keyPressed() {
-    let number = random(10, 20)
-    for (let i = 0; i < number; i++) {
-        let Size = random(10, 50)
-        let circle_col = color(random(0, 255), random(0, 255), random(0, 255))
-        let circlenum = random(5, 20)
-        let circ_x = random(0, 400)
-        let circ_y = random(0, 400)
-        fill(circle_col)
-        ellipse(circ_x, circ_y, Size, Size)
-    }
-}
-function keyReleased() {
-    background(220)
-}
+// function keyPressed() {
+//     let number = random(10, 20)
+//     for (let i = 0; i < number; i++) {
+//         let Size = random(10, 50)
+//         let circle_col = color(random(0, 255), random(0, 255), random(0, 255))
+//         let circlenum = random(5, 20)
+//         let circ_x = random(0, 400)
+//         let circ_y = random(0, 400)
+//         fill(circle_col)
+//         ellipse(circ_x, circ_y, Size, Size)
+//     }
+// }
+// function keyReleased() {
+//     background(220)
+// }
