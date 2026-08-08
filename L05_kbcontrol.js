ebe90bd88fc1e0152129b1b88bbcf4928880d74a
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-08 07:25:00 +0000

    Aug 8, 2026, 7:25 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -39,7 +39,7 @@ function keyPressed() {
         circle(200, 200, 300);
         console.log('heo')
     }
-    if (key === 's') {
+    if (keycode === ) {
         rect(100, 100, 200, 200);
     }
 }
