d3cc297cbe6536d27c0fb9fa81f58701d3077e29
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-22 06:53:39 +0000

    Aug 22, 2026, 6:53 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,53 +1,53 @@
-// function setup() {
-//     createCanvas(400, 400);
-//     background(220)
-// }
-// let x = 200
-// let y = 200
-// function draw() {
-//     background(220)
-//        if (keyIsDown(UP_ARROW)) {
-//         y -= 2;
-//     }
-//     if (keyIsDown(DOWN_ARROW)) {
-//         y += 2;
-//     }
-//     if (keyIsDown(RIGHT_ARROW)) {
-//         x  += 2;
-//     }
-//     if (keyIsDown(LEFT_ARROW)) {
-//         x  -= 2;
-//     }
-//     if (keyCode === 32) {
-//         let col = random(0, 255);
-//         fill(col);
-//     }
-
-//     y = constrain(y, 50, 350 - 50);
-//     x = constrain(x, 50, 350 - 50);
-//     rect(x, y, 50, 50);
-// }
-
-let soundEffect, bgMusic, staticImage;
-
-function preload() {
-    soundEffect = loadSound('assets/pop.mp3');
-    mgMusic = loadSound('assests/bossNova.mp3');
-    staticImage = loadImage('assets/pico-a.png');
-}
-
 function setup() {
-    createCanvas(200, 200);
-    bgMusic.loop();
+    createCanvas(400, 400);
+    background(220)
 }
-
+let x = 200
+let y = 200
 function draw() {
-    background("lightblue")
-    image(staticImage, 0, 0, 110, 113);
+    background(220)
+       if (keyIsDown(UP_ARROW)) {
+        y -= 2;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        y += 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        x  += 2;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+        x  -= 2;
+    }
+    if (keyCode === 32) {
+        let col = random(0, 255);
+        fill(col);
+    }
+
+    y = constrain(y, 50, 350 - 50);
+    x = constrain(x, 50, 350 - 50);
+    rect(x, y, 50, 50);
 }
 
-function keyPressed() {
-    if (keyCode === 32) {
-        soundEffect.play();
-    }
-}
+// let soundEffect, bgMusic, staticImage;
+
+// function preload() {
+//     soundEffect = loadSound('assets/pop.mp3');
+//     mgMusic = loadSound('assests/bossNova.mp3');
+//     staticImage = loadImage('assets/pico-a.png');
+// }
+
+// function setup() {
+//     createCanvas(200, 200);
+//     bgMusic.loop();
+// }
+
+// function draw() {
+//     background("lightblue")
+//     image(staticImage, 0, 0, 110, 113);
+// }
+
+// function keyPressed() {
+//     if (keyCode === 32) {
+//         soundEffect.play();
+//     }
+// }
