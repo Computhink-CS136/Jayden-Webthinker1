ccb20d01815798b3085eef6abb0e133c49db7d51
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-22 07:02:53 +0000

    Aug 22, 2026, 7:02 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,7 +1,7 @@
-// function setup() {
-//     createCanvas(400, 400);
-//     background(220)
-// }
+function setup() {
+    createCanvas(400, 400);
+    background(220)
+}
 // let x = 200
 // let y = 200
 // function draw() {
