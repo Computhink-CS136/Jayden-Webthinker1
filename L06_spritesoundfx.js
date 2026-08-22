eba47d06a81d923162d4397736708a3e1a409803
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-22 06:23:57 +0000

    Aug 22, 2026, 6:23 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -9,10 +9,11 @@ function draw() {
     background(220)
        if (keyIsDown(UP_ARROW)) {
         y -= 2;
-        direction = "up"
+        direction = "up";
     }
     if (keyIsDown(DOWN_ARROW)) {
         y += 2;
+        direction = "up";
     }
     if (keyIsDown(RIGHT_ARROW)) {
         x  += 2;
