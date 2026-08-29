effac57cb71af7ffa5b709dae5e87ca816a2c5d4
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-29 06:52:39 +0000

    Aug 29, 2026, 6:52 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,45 +1,45 @@
-let picoImage;
-let popSound;
+// let picoImage;
+// let popSound;
 
-function preload() {
-    picoImage = loadImage("assets/pico-a.png")
-    popSound = loadSound("assets/pop.mp3")
-}
+// function preload() {
+//     picoImage = loadImage("assets/pico-a.png")
+//     popSound = loadSound("assets/pop.mp3")
+// }
 
-function setup() {
-    createCanvas(600, 400);
-}
-let picoWidth = 100;
-let speed = 5;
-let x = 100;
-let y = 200;
-function draw() {
-    background("gray");
-    if (keyIsDown(UP_ARROW)) {
-        y -= speed;
-    }
-    if (keyIsDown(DOWN_ARROW)) {
-        y += speed;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-        x  += speed;
-    }
-    if (keyIsDown(LEFT_ARROW)) {
-        x  -= speed;
-    }
-    y = constrain(y, 0, height - 100);
-    x = constrain(x, 0, width - 100);
-    image(picoImage, x, y, picoWidth, 100);
-}
-function keyPressed() {
-    if (keyCode === 32) {
-        popSound.play();
-        picoWidth = 50;
-    }
-}
+// function setup() {
+//     createCanvas(600, 400);
+// }
+// let picoWidth = 100;
+// let speed = 5;
+// let x = 100;
+// let y = 200;
+// function draw() {
+//     background("gray");
+//     if (keyIsDown(UP_ARROW)) {
+//         y -= speed;
+//     }
+//     if (keyIsDown(DOWN_ARROW)) {
+//         y += speed;
+//     }
+//     if (keyIsDown(RIGHT_ARROW)) {
+//         x  += speed;
+//     }
+//     if (keyIsDown(LEFT_ARROW)) {
+//         x  -= speed;
+//     }
+//     y = constrain(y, 0, height - 100);
+//     x = constrain(x, 0, width - 100);
+//     image(picoImage, x, y, picoWidth, 100);
+// }
+// function keyPressed() {
+//     if (keyCode === 32) {
+//         popSound.play();
+//         picoWidth = 50;
+//     }
+// }
 
-function keyReleased() {
-    if (keyCode=== 32) {
-        picoWidth = 100;
-    }
-}
+// function keyReleased() {
+//     if (keyCode=== 32) {
+//         picoWidth = 100;
+//     }
+// }
