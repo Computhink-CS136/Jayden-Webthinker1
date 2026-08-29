03203f23b582f163bac3d3c3c2e53df4a1c7600a
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-29 06:29:15 +0000

    Aug 29, 2026, 6:29 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -26,8 +26,6 @@ function draw() {
     if (keyIsDown(LEFT_ARROW)) {
         x  -= speed;
     }
-    if (x === 400 - 100 || x === 0 || y === 400  - 100|| y === 0 ) {
-    }
     y = constrain(y, 0, width - 100);
     x = constrain(x, 0, height - 100);
     image(picoImage, x, y, 100, 100);
