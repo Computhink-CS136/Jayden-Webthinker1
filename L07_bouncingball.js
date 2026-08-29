58136ee48769e0005609b33f3d2655cbb8f712d8
Computhink-CS136 <cs136@computhink.com.sg>
2026-08-29 06:24:50 +0000

    Aug 29, 2026, 6:24 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -9,27 +9,8 @@ function preload() {
 function setup() {
     createCanvas(600, 400);
 }
-let speed = 5;
-let x = 100;
-let y = 200;
+
 function draw() {
     background("gray");
-    if (keyIsDown(UP_ARROW)) {
-        y -= speed;
-    }
-    if (keyIsDown(DOWN_ARROW)) {
-        y += speed;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-        x  += speed;
-    }
-    if (keyIsDown(LEFT_ARROW)) {
-        x  -= speed;
-    }
-    if (x === 400 - 100 || x === 0 || y === 400  - 100|| y === 0 ) {
-        soundEffect.play();
-        background("red")
-    }
-    y = constrain(y, 0, width - 100);
-    x = constrain(x, 0, height - 100);
     image(staticImage, x, y, 100, 100);
+}
