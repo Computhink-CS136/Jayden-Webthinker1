30924e2e9a15551f85adb6be82ad3f96d4397ea4
Computhink-CS136 <cs136@computhink.com.sg>
2026-09-12 06:56:04 +0000

    Sep 12, 2026, 6:56 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -38,6 +38,7 @@ function setup() {
     background(220);
 
     textSize(40);
-    text("My name is Jayden", 50, 50)
-    text("My name is Jayden", 50, 50)
+    text("My name is Jayden", 50, 50);
+    text("I am 13 this year", 50, 50);
+    
 }
