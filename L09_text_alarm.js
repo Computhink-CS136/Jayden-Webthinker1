35a382b1145a760dab8c8a7b06eca997edb588f3
Computhink-CS136 <cs136@computhink.com.sg>
2026-09-19 07:33:35 +0000

    Sep 19, 2026, 7:33 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -15,11 +15,6 @@ function draw() {
     min = minute();
     sec = second();
 
-
-    text(hr, width / 2, height / 2);
-    text(min, width / 2, height / 2 + 50);
-    text(sec, width / 2, height / 2 + 100);
-
-        timeString = nf(hr, 2) + ":" + nf(min, 2) + ":" + nf(sec, 2)
+    timeString = nf(hr, 2) + ":" + nf(min, 2) + ":" + nf(sec, 2)
         text(timeString, width / 2, height / 2);
 }
