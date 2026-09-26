88d49c994cbcf2dba40a651d73f116d5b6711389
Computhink-CS136 <cs136@computhink.com.sg>
2026-09-26 07:18:33 +0000

    Sep 26, 2026, 7:18 AM

--- L10_name.js
+++ L10_name.js
@@ -4,5 +4,6 @@ let displayText = "Input your name";
 function setup() {
     createCanvas(600, 400);
     inputText = createInput();
-    inputText.position(width)
+    inputText.position(width / 2, height / 2);
+    
 }
