e18ad7efc67a0f22b20a60185579aa89b1670a8c
Computhink-CS136 <cs136@computhink.com.sg>
2026-09-26 06:53:23 +0000

    Sep 26, 2026, 6:53 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -58,7 +58,7 @@ function draw() {
     text(countdown, width / 2, height / 2);
 }
 function mousePressed() {
-    inte
+    intervalId = setInterval(updateCountdown, 1000);
 }
 function updateCountdown() {
     countdown -- ;
