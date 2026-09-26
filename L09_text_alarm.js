4e314e86299ee19b0be34b05c12371081c3e8dff
Computhink-CS136 <cs136@computhink.com.sg>
2026-09-26 06:07:16 +0000

    Sep 26, 2026, 6:07 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,20 +1,20 @@
-let hr;
-let min;
-let sec;
+// let hr;
+// let min;
+// let sec;
 
-function setup() {
-    createCanvas(600, 400);
-    textAlign(CENTER, CENTER);
-    textSize(40);
-    fill("rgb(20, 70, 190)");
-}
+// function setup() {
+//     createCanvas(600, 400);
+//     textAlign(CENTER, CENTER);
+//     textSize(40);
+//     fill("rgb(20, 70, 190)");
+// }
 
-function draw() {
-    background(0)
-    hr = hour();
-    min = minute();
-    sec = second();
+// function draw() {
+//     background(0)
+//     hr = hour();
+//     min = minute();
+//     sec = second();
 
-    timeString = nf(hr, 2) + ":" + nf(min, 2) + ":" + nf(sec, 2)
-    text(timeString, width / 2, height / 2);
-}
+//     timeString = nf(hr, 2) + ":" + nf(min, 2) + ":" + nf(sec, 2)
+//     text(timeString, width / 2, height / 2);
+// }
